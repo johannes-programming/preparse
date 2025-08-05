51337f62ef8ab811d80e77f8441f8ac24ae33480
Johannes <johannes-programming@mailfence.com>
2025-08-05 20:43:37 +0200

    Version 0.1.0.dev5

--- src/preparse/_parsing/process.py
+++ src/preparse/_parsing/process.py
@@ -1,27 +1,28 @@
-#from preparse._parsing.parse import *
-#from preparse._parsing.digest import *
-#from preparse._parsing.deparse import *
-#from preparse._parsing.Item import *
-from preparse._parsing.Parsing import *
-from typing import *
 import sys
+from typing import *
+
+from preparse._parsing.deparse import *
+from preparse._parsing.digest import *
+from preparse._parsing.Item import *
+from preparse._parsing.parse import *
 
 if TYPE_CHECKING:
     from preparse.core.PreParser import PreParser
 
 __all__ = ["process"]
 
+
 def process(
     *,
     args: Optional[Iterable] = None,
-    parser:"PreParser",
+    parser: "PreParser",
 ) -> list[str]:
     "This method parses args."
     if args is None:
         args = sys.argv[1:]
-    args=[str(a) for a in args]
+    args = [str(a) for a in args]
     parser = parser.copy()
-    #items:list[Item] = parse(args=args, parser=parser)
-    #items:list[Item] = digest(items=items, parser=parser)
-    #ans:list[str] = deparse(items=items, parser=parser)
-    return Parsing(parser=parser,args=args).ans
+    items: list[Item] = list(parse(args=args, parser=parser))
+    items: list[Item] = list(digest(items=items, parser=parser))
+    ans: list[str] = list(deparse(items=items))
+    return ans
