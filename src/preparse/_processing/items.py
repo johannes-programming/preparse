c5ead3aee32fdad38c760dbd2afc3eada574a7e9
Johannes <johannes-programming@mailfence.com>
2025-08-13 15:37:50 +0200

    Version 0.2.0+master.abc

--- src/preparse/_processing/items.py
+++ src/preparse/_processing/items.py
@@ -1,47 +1,65 @@
 import abc
 import dataclasses
+import operator
 from typing import *
+
+import makeprop
 
 from preparse.core.enums import *
 from preparse.core.warnings import *
 
-__all__ = ["Item"]
+__all__ = ["Item", "Option", "Special", "Positional"]
 
 
-class BaseItem(abc.ABC):
+class Item(abc.ABC):
     @abc.abstractmethod
     def deparse(self: Self) -> list[str]: ...
 
+    @classmethod
+    @abc.abstractmethod
+    def sortkey(cls: type) -> int: ...
 
-@dataclasses.dataclass
-class Item(BaseItem):
-    key: Optional[str] = None
-    remainder: bool | str = False
-    value: Optional[str] = None
+
+class Option(Item):
+    __slots__ = ("_key", "_remainder", "_value")
+
+    def __init__(
+        self: Self,
+        *,
+        key: str,
+        remainder: bool | str = False,
+        value: Optional[str] = None,
+    ) -> None:
+        self.key = key
+        self.remainder = remainder
+        self.value = value
+
+    @makeprop.makeprop()
+    def key(self: Self, x: Any) -> str:
+        return str(x)
+
+    @makeprop.makeprop()
+    def remainder(self: Self, x: Any) -> bool | str:
+        try:
+            return bool(operator.index(x))
+        except:
+            return str(x)
+
+    @makeprop.makeprop()
+    def value(self: Self, x: Any) -> Optional[str]:
+        if x is not None:
+            return str(x)
 
     def ishungry(self: Self) -> bool:
         return self.remainder and (self.value is None)
 
-    def isoption(self: Self) -> bool:
-        return self.key is not None
-
     def islong(self: Self) -> bool:
-        return self.isoption() and self.key.startswith("-")
+        return self.key.startswith("-")
 
     def isbundle(self: Self) -> bool:
-        return self.isoption() and not self.key.startswith("-")
-
-    def isspecial(self: Self) -> bool:
-        return self.key is None and self.value is None
-
-    def ispositional(self: Self) -> bool:
-        return self.key is None and self.value is not None
+        return not self.key.startswith("-")
 
     def deparse(self: Self) -> list[str]:
-        if self.isspecial():
-            return ["--"]
-        if self.ispositional():
-            return [self.value]
         if self.isbundle():
             if self.value is None:
                 return ["-" + self.key]
@@ -56,3 +74,36 @@ class Item(BaseItem):
                 return [self.key + "=" + self.value]
             else:
                 return [self.key, self.value]
+
+    @classmethod
+    def sortkey(cls: type) -> int:
+        return 0
+
+
+class Special(Item):
+    __slots__ = ()
+
+    def deparse(self: Self) -> list[str]:
+        return ["--"]
+
+    @classmethod
+    def sortkey(cls: type) -> int:
+        return 1
+
+
+class Positional(Item):
+    __slots__ = ("_value",)
+
+    def __init__(self: Self, value: Any) -> None:
+        self.value = value
+
+    @makeprop.makeprop()
+    def value(self: Self, x: Any) -> str:
+        return str(x)
+
+    def deparse(self: Self) -> list[str]:
+        return [self.value]
+
+    @classmethod
+    def sortkey(cls: type) -> int:
+        return 2
