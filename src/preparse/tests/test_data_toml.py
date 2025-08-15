34bc0c41bb5080ef9e2d74302dfcf692399a03c4
Johannes <johannes.programming@gmail.com>
2025-08-15 08:16:41 +0200

    Version 0.2.0+master.warnings

--- src/preparse/tests/test_data_toml.py
+++ src/preparse/tests/test_data_toml.py
@@ -1,38 +1,50 @@
+import math
 import tomllib
 import unittest
 from importlib import resources
+from typing import *
 
 from preparse.core import *
-from typing import *
-import math
 
 # Read URL of the Real Python feed from config file
 
 
 class TestDataToml(unittest.TestCase):
 
-    def get_data(self:Self)->dict:
+    def get_data(self: Self) -> dict:
         text: str = resources.read_text("preparse.tests", "data.toml")
         data: dict = tomllib.loads(text)
         return data
 
-    def test_0(self:Self)->None:
+    def test_0(self: Self) -> None:
         data: dict = self.get_data()
         data = data["data"]
         for kwargs in data:
             self.parse(**kwargs)
 
     def parse(
-        self:Self,
+        self: Self,
         *,
-        query,
-        solution,
-        warnings,
-        **kwargs,
-    ):
-        parser = PreParser(**kwargs)
+        query: Any,
+        solution: Any,
+        warnings: Any,
+        **kwargs: Any,
+    ) -> None:
+        capture: list = list()
+
+        def warn(value: Any) -> None:
+            capture.append(str(value))
+
+        parser = PreParser(warn=warn, **kwargs)
         msg: str = "parser=%r, query=%r" % (parser, query)
         answer = parser.parse_args(query)
+        erranswer: list = list(capture)
+        capture.clear()
         superanswer = parser.parse_args(answer)
+        supererranswer: list = list(capture)
+        capture.clear()
         self.assertEqual(answer, superanswer, msg=msg)
+        # self.assertEqual(erranswer, supererranswer, msg=msg)
         self.assertEqual(answer, solution, msg=msg)
+        if not math.isnan(warnings):
+            self.assertEqual(erranswer, warnings, msg=msg)
