25e59bbe499294c284850661bb3aacbd5a2378eb
Johannes <johannes.programming@gmail.com>
2025-08-31 12:16:37 +0200

    Version 0.1.8

--- src/preparse/tests/test_features.py
+++ src/preparse/tests/test_features.py
@@ -6,17 +6,17 @@ from preparse.core import *
 
 class TestPreparse(unittest.TestCase):
 
-    def test_nargs_enum(self: Self):
+    def test_nargs_enum(self: Self) -> None:
         self.assertEqual(Nargs.NO_ARGUMENT, 0)
         self.assertEqual(Nargs.REQUIRED_ARGUMENT, 1)
         self.assertEqual(Nargs.OPTIONAL_ARGUMENT, 2)
 
-    def test_preparser_copy(self: Self):
+    def test_preparser_copy(self: Self) -> None:
         parser: PreParser = PreParser()
         parser_copy: PreParser = parser.copy()
         self.assertEqual(parser.optdict, parser_copy.optdict)
 
-    def test_preparser_todict(self: Self):
+    def test_preparser_todict(self: Self) -> None:
         parser: PreParser = PreParser()
         result: Any = parser.todict()
         expected_keys: list = [
@@ -25,7 +25,7 @@ class TestPreparse(unittest.TestCase):
         ]
         self.assertTrue(all(key in result for key in expected_keys))
 
-    def test_preparser_click_decorator(self: Self):
+    def test_preparser_click_decorator(self: Self) -> None:
         parser: PreParser = PreParser()
         click_decorator: Click = parser.click()
         self.assertIsInstance(click_decorator, Click)
