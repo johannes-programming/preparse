dcf03ff3263919f72f10c13eb6cd103b850fb91f
Johannes <johannes.programming@gmail.com>
2025-12-28 10:56:49 +0100

    Version 0.1.12+local

--- src/preparse/_processing/items/Special.py
+++ src/preparse/_processing/items/Special.py
@@ -1,12 +1,6 @@
-import abc
-import operator
 from typing import *
 
-import setdoc
-from datarepr import datarepr
-
 from preparse._processing.items.Item import Item
-from preparse._utils.dataprop import dataprop
 from preparse.core.enums import *
 
 __all__ = ["Special"]
