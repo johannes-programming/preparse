76b236af0c22de15dbae3a1dc51c15f52cb00716
Johannes <johannes.programming@gmail.com>
2025-12-28 11:17:13 +0100

    Version 0.1.12+local

--- src/preparse/_processing/parsing.py
+++ src/preparse/_processing/parsing.py
@@ -1,7 +1,12 @@
 from types import FunctionType
 from typing import *
 
-from preparse._processing.items import *
+from preparse._processing.items.Bundle import Bundle
+from preparse._processing.items.Item import Item
+from preparse._processing.items.Long import Long
+from preparse._processing.items.Option import Option
+from preparse._processing.items.Positional import Positional
+from preparse._processing.items.Special import Special
 from preparse.core import warnings
 from preparse.core.enums import *
 
