7c4be89755f058eebb1de2296cf347158cad6e53
Johannes <johannes-programming@mailfence.com>
2026-04-21 17:17:54 +0200

    Version 1.0.11

--- src/preparse/_items/Long.py
+++ src/preparse/_items/Long.py
@@ -3,7 +3,7 @@ from typing import *
 
 import setdoc
 
-from preparse._processing.items.Option import Option
+from preparse._items.Option import Option
 from preparse._utils.dataprop import dataprop
 from preparse.core.enums import *
 
