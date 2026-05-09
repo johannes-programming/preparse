0dd658734bde310e1fde6340aabbe339c356eee2
Johannes <johannes.programming@gmail.com>
2026-05-09 13:46:14 +0200

    Version 2.0.0.dev20+local.3

--- src/preparse/enums/BaseEnum.py
+++ src/preparse/enums/BaseEnum.py
@@ -4,6 +4,7 @@ the value of two represents always an intermediary answer \
 between the values zero, meaning no, and one, meaning yes."""
 
 import enum
+import operator
 from typing import *
 
 __all__ = ["BaseEnum"]
@@ -11,5 +12,6 @@ __all__ = ["BaseEnum"]
 
 class BaseEnum(enum.IntEnum):
     @classmethod
-    def _missing_(cls: type, value: Any) -> Self:
+    def _missing_(cls: type, value: SupportsIndex) -> Self:
+        operator.index(value)
         return cls(2)
